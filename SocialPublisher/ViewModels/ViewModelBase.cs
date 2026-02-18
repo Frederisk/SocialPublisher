c4e07ad9ead39a34b74f00538cedda681e33b986
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2026-02-19 00:34:21 +0800

    Add multi-platform Avalonia SocialPublisher

---
+++ SocialPublisher/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SocialPublisher.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ SocialPublisher/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SocialPublisher.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
